2d5ee9ed116f7d8cc708ae2075e7feace1af8a09
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 20:06:58 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,23 +1,17 @@
 class Cat
-  attr_accessor :owner, :mood, :collection
+  attr_accessor :owner, :mood
   attr_reader :name
 
   @@all = []
-  @collection = []
 
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
     @@all << self
-    @collection << name
   end
 
   def self.all
     @@all
   end
-
-  def owner_collection
-    @collection
-  end 
 end
