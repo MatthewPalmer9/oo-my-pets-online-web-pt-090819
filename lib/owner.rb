0910c7c7b69afc77ff953dc3277e379af8c8191d
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 18:51:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,10 @@
 class Owner
-  attr_accessor :owner
+  attr_accessor :owner, :species
   attr_reader :name
 
   def initialize(name)
     @name = name
     @owner = owner
+    @species = human
   end
 end
