3c7a1376e3b7173ff899b0a6ec4883a573e6e61d
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 21:08:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,6 +64,6 @@ class Owner
   end
 
   def list_pets
-    "I have #{Dog.all.length} dog(s), and #{Cat.all.length} cat(s)."
+    "I have #{self.all.count} dog(s), and #{self.all.count} cat(s)."
   end
 end
