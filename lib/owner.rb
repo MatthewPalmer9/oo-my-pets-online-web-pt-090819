daea14537b3720ac88e69f637c37481ee7e35eea
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 20:08:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,11 +33,6 @@ class Owner
   end
 
   def cats
-    Cat.all.collect(){|cat|
-      if cat.owner == nil
-        cat.owner = self
-      end
-      cat
-    }
+    Cat.all.collect(){|cat| cat if cat.owner == self }
   end
 end
