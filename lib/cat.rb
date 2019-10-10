7775eac560ad54d1faebfe057ad0e31d95d5e6bb
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 19:28:06 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,9 +2,8 @@ class Cat
   attr_accessor :owner
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, owner)
     @name = name
     @owner = owner
-    owner = Owner.new(name)
   end
 end
